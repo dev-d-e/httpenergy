74eb389f9c4eade3d2f8d65ec6609201e132d313
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2024-01-15 22:07:53 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -15,21 +15,31 @@
 #![allow(dead_code)]
 
 mod common;
+#[macro_use]
 mod request;
 mod response;
 
-use common::*;
 pub use request::*;
 pub use response::*;
 
-///Parse request data
-pub fn to_request(bytes: &[u8]) -> Request {
+///Parse request bytes to `RequestUnits`.
+pub fn to_request(bytes: &[u8]) -> RequestUnits {
     RequestBuilder::new().from_bytes(bytes)
 }
 
-///Parse response data
-pub fn to_response(bytes: &[u8]) -> Response {
+///Parse request bytes to `Request`.
+pub fn into_request(bytes: Vec<u8>) -> Request {
+    RequestBuilder::new().from_vec(bytes)
+}
+
+///Parse response bytes to `ResponseUnits`.
+pub fn to_response(bytes: &[u8]) -> ResponseUnits {
     ResponseBuilder::new().from_bytes(bytes)
+}
+
+///Parse response bytes to `Response`.
+pub fn into_response(bytes: Vec<u8>) -> Response {
+    ResponseBuilder::new().from_vec(bytes)
 }
 
 #[cfg(test)]
@@ -38,19 +48,22 @@ mod tests {
 
     #[test]
     fn test_request() {
-        let s = "GET / HTTP1.1\r\n";
+        let s =
+            "GET / HTTP1.1\r\nAccept: text/*, text/plain, text/plain;format=flowed, */*\r\n\r\n";
         let mut r = to_request(s.as_bytes());
         let m = r.method().to_string();
-        println!("{:?}\n{:?}", m, r);
+        let v = r.header_value_string("Accept");
+        println!("{:?}  {:?}", m, v);
         assert_eq!(m, "GET");
     }
 
     #[test]
     fn test_response() {
-        let s = "HTTP1.1 200 \r\n";
+        let s = "HTTP1.1 200 \r\nAccept: text/*, text/plain, text/plain;format=flowed, */*\r\n\r\n";
         let mut r = to_response(s.as_bytes());
         let c = r.status_code().to_string();
-        println!("{:?}\n{:?}", c, r);
+        let v = r.header_value_string("Accept");
+        println!("{:?}  {:?}", c, v);
         assert_eq!(c, "200");
     }
 }
