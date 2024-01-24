726ef8d02c189bd43968911a2c39963c10ab5a1a
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2024-01-24 16:34:18 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -22,48 +22,50 @@ mod response;
 pub use request::*;
 pub use response::*;
 
-///Parse request bytes to `RequestUnits`.
-pub fn to_request(bytes: &[u8]) -> RequestUnits {
-    RequestBuilder::new().from_bytes(bytes)
-}
-
-///Parse request bytes to `Request`.
-pub fn into_request(bytes: Vec<u8>) -> Request {
-    RequestBuilder::new().from_vec(bytes)
-}
-
-///Parse response bytes to `ResponseUnits`.
-pub fn to_response(bytes: &[u8]) -> ResponseUnits {
-    ResponseBuilder::new().from_bytes(bytes)
-}
-
-///Parse response bytes to `Response`.
-pub fn into_response(bytes: Vec<u8>) -> Response {
-    ResponseBuilder::new().from_vec(bytes)
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_request() {
-        let s =
-            "GET / HTTP1.1\r\nAccept: text/*, text/plain, text/plain;format=flowed, */*\r\n\r\n";
-        let mut r = to_request(s.as_bytes());
-        let m = r.method().to_string();
-        let v = r.header_value_string("Accept");
-        println!("{:?}  {:?}", m, v);
-        assert_eq!(m, "GET");
+        let mut s = Vec::from(
+            "GET / HTTP1.1\r\nAccept: text/*, text/plain, text/plain;format=flowed, */*\r\n",
+        );
+        let mut r = new_request_units(&s);
+        println!("Accept: {:?}", r.header_value_string("Accept", &s));
+        s.extend_from_slice("Host:www\r\n\r\n ".as_bytes());
+        if r.is_finish() {
+            r = new_request_units(&s);
+        }
+        println!("Host: {:?}", r.header_value_string("Host", &s));
+
+        let mut r = pack_request("GET", "/");
+        r.set_header(
+            String::from("Accept"),
+            Vec::from("text/*, text/plain, text/plain;format=flowed, */*"),
+        );
+        let s = r.pack();
+        let mut r = to_request(s);
+        println!("Accept: {:?}", r.header_value_string("Accept"));
+        assert_eq!(r.method(), "GET");
     }
 
     #[test]
     fn test_response() {
-        let s = "HTTP1.1 200 \r\nAccept: text/*, text/plain, text/plain;format=flowed, */*\r\n\r\n";
-        let mut r = to_response(s.as_bytes());
-        let c = r.status_code().to_string();
-        let v = r.header_value_string("Accept");
-        println!("{:?}  {:?}", c, v);
-        assert_eq!(c, "200");
+        let s = Vec::from(
+            "HTTP1.1 200 \r\nAccept: text/*, text/plain, text/plain;format=flowed, */*\r\n\r\n",
+        );
+        let mut r = new_response_units(&s);
+        println!("Accept: {:?}", r.header_value_string("Accept", &s));
+
+        let mut r = pack_response("200");
+        r.set_header(
+            String::from("Accept"),
+            Vec::from("text/*, text/plain, text/plain;format=flowed, */*"),
+        );
+        let s = r.pack();
+        let mut r = to_response(s);
+        println!("Accept: {:?}", r.header_value_string("Accept"));
+        assert_eq!(r.status_code(), "200");
     }
 }
