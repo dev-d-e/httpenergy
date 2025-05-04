0160e574b47d32c43320e788e528cc9008227f45
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2025-05-04 22:33:28 +0800

    Update mod.rs

--- src/h2/hpack/mod.rs
+++ src/h2/hpack/mod.rs
@@ -1,8 +1,70 @@
 mod index;
 
 use super::prty::*;
-use bytes::{Buf, BufMut};
+use crate::{ReadByte, WriteByte};
 pub use index::*;
+
+///Represents header field.
+pub enum FieldRepresentation<'a> {
+    Indexed(usize),
+    IndexedNameValue(FieldIndexType, usize, &'a [u8]),
+    NewNameValue(FieldIndexType, &'a [u8], &'a [u8]),
+}
+
+impl<'a> FieldRepresentation<'a> {
+    pub fn encode(self, writer: &mut impl WriteByte) {
+        match self {
+            Self::Indexed(n) => {
+                EncodeInstructions::indexed(n, writer);
+            }
+            Self::IndexedNameValue(t, n, value) => {
+                t.indexed_name(n, value, writer);
+            }
+            Self::NewNameValue(t, name, value) => {
+                t.new_name(name, value, writer);
+            }
+        }
+    }
+}
+
+///Represents index type of header field.
+pub enum FieldIndexType {
+    IncrementalIndexing,
+    WithoutIndexing,
+    NeverIndexed,
+}
+
+impl FieldIndexType {
+    ///Indexed Name
+    pub fn indexed_name(self, n: usize, value: &[u8], writer: &mut impl WriteByte) {
+        match self {
+            Self::IncrementalIndexing => {
+                EncodeInstructions::incremental_indexing_indexed_name(n, value, writer);
+            }
+            Self::WithoutIndexing => {
+                EncodeInstructions::without_indexing_indexed_name(n, value, writer);
+            }
+            Self::NeverIndexed => {
+                EncodeInstructions::never_indexed_indexed_name(n, value, writer);
+            }
+        }
+    }
+
+    ///New Name
+    pub fn new_name(self, name: &[u8], value: &[u8], writer: &mut impl WriteByte) {
+        match self {
+            Self::IncrementalIndexing => {
+                EncodeInstructions::incremental_indexing_new_name(name, value, writer);
+            }
+            Self::WithoutIndexing => {
+                EncodeInstructions::without_indexing_new_name(name, value, writer);
+            }
+            Self::NeverIndexed => {
+                EncodeInstructions::never_indexed_new_name(name, value, writer);
+            }
+        }
+    }
+}
 
 ///Process index or literal header field to instructions.
 ///Include different header field representations and the dynamic table size update instruction.
@@ -11,51 +73,51 @@ pub struct EncodeInstructions;
 impl EncodeInstructions {
     ///Indexed Header Field Representation.
     ///An indexed header field representation identifies an entry in either the static table or the dynamic table.
-    pub fn indexed(n: usize, writer: &mut impl BufMut) {
+    pub fn indexed(n: usize, writer: &mut impl WriteByte) {
         encode_integer(n, 7, 0x80, writer);
     }
 
     ///Literal Header Field with Incremental Indexing -- Indexed Name
-    pub fn incremental_indexing_indexed_name(n: usize, value: &[u8], writer: &mut impl BufMut) {
+    pub fn incremental_indexing_indexed_name(n: usize, value: &[u8], writer: &mut impl WriteByte) {
         encode_integer(n, 6, 0x40, writer);
         encode_literal_huffman_encoded(value, writer);
     }
 
     ///Literal Header Field with Incremental Indexing -- New Name
-    pub fn incremental_indexing_new_name(name: &[u8], value: &[u8], writer: &mut impl BufMut) {
-        writer.put_u8(0x40);
+    pub fn incremental_indexing_new_name(name: &[u8], value: &[u8], writer: &mut impl WriteByte) {
+        writer.put(0x40);
         encode_literal_huffman_encoded(name, writer);
         encode_literal_huffman_encoded(value, writer);
     }
 
     ///Literal Header Field without Indexing -- Indexed Name
-    pub fn without_indexing_indexed_name(n: usize, value: &[u8], writer: &mut impl BufMut) {
+    pub fn without_indexing_indexed_name(n: usize, value: &[u8], writer: &mut impl WriteByte) {
         encode_integer(n, 4, 0x00, writer);
         encode_literal_huffman_encoded(value, writer);
     }
 
     ///Literal Header Field without Indexing -- New Name
-    pub fn without_indexing_new_name(name: &[u8], value: &[u8], writer: &mut impl BufMut) {
-        writer.put_u8(0x00);
+    pub fn without_indexing_new_name(name: &[u8], value: &[u8], writer: &mut impl WriteByte) {
+        writer.put(0x00);
         encode_literal_huffman_encoded(name, writer);
         encode_literal_huffman_encoded(value, writer);
     }
 
     ///Literal Header Field Never Indexed -- Indexed Name
-    pub fn never_indexed_indexed_name(n: usize, value: &[u8], writer: &mut impl BufMut) {
+    pub fn never_indexed_indexed_name(n: usize, value: &[u8], writer: &mut impl WriteByte) {
         encode_integer(n, 4, 0x10, writer);
         encode_literal_huffman_encoded(value, writer);
     }
 
     ///Literal Header Field Never Indexed -- New Name
-    pub fn never_indexed_new_name(name: &[u8], value: &[u8], writer: &mut impl BufMut) {
-        writer.put_u8(0x10);
+    pub fn never_indexed_new_name(name: &[u8], value: &[u8], writer: &mut impl WriteByte) {
+        writer.put(0x10);
         encode_literal_huffman_encoded(name, writer);
         encode_literal_huffman_encoded(value, writer);
     }
 
     ///Maximum Dynamic Table Size Change
-    pub fn dynamic_table_size_update(n: usize, writer: &mut impl BufMut) {
+    pub fn dynamic_table_size_update(n: usize, writer: &mut impl WriteByte) {
         encode_integer(n, 5, 0x20, writer);
     }
 }
@@ -92,12 +154,12 @@ pub trait DecodeInstructions {
 ///Decode a byte slice.
 pub fn decode(mut buffer: &[u8], ins: &mut impl DecodeInstructions) {
     let reader = &mut buffer;
-    while reader.has_remaining() {
-        decode_u8(reader.get_u8(), reader, ins);
+    while let Some(i) = reader.fetch() {
+        decode_u8(i, reader, ins);
     }
 }
 
-fn decode_u8(i: u8, reader: &mut impl Buf, ins: &mut impl DecodeInstructions) {
+fn decode_u8(i: u8, reader: &mut impl ReadByte, ins: &mut impl DecodeInstructions) {
     match i {
         129..255 => {
             ins.indexed((i & 0x7f) as usize);
