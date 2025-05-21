35c41ac5729b152f7f9b09bf50167ef2095118c8
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2025-05-21 17:15:46 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,33 @@
-//!A library for parsing HTTP request and response.
-//!
-//!```
-//!use httpenergy::{H1Request, H1RequestUnits, H1RequestDecoder};
-//!
-//!let request = H1Request::with_method_target("GET", "/");
-//!let mut s = Vec::new();
-//!request.export(&mut s);
-//!
-//!let units=H1RequestUnits::new(&s);
-//!let mut r =H1Request::new();
-//!units.copy_to_request(&mut r);
-//!println!("H1RequestUnits: {:?}", r.method());
-//!
-//!let decoder = H1RequestDecoder::new(s);
-//!let r2 =decoder.to_request();
-//!println!("H1RequestDecoder: {:?}", r2.method());
-//!
-//!```
-//!
+/*!
+Httpenergy is a crate for working with request and response format in HTTP/1.1, HTTP/2 and HTTP/3 protocols.
+
+This crate does not provide anything about connection.
+
+# Examples
+```
+use httpenergy::{H1Request, H1RequestUnits, H1RequestDecoder};
+//Creates HTTP/1.1 request.
+let request = H1Request::with_method_target("GET", "/");
+let mut s = Vec::new();
+request.export(&mut s);
+
+//There are two ways for parsing request bytes.
+let units=H1RequestUnits::new(&s);
+let mut r =H1Request::new();
+units.copy_to_request(&mut r);
+println!("H1RequestUnits: {:?}", r.method());
+
+let decoder = H1RequestDecoder::new(s);
+let r2 =decoder.to_request();
+println!("H1RequestDecoder: {:?}", r2.method());
+```
+*/
 
 #![allow(dead_code)]
 
 mod common;
 pub mod h2;
+pub mod h3;
 mod io;
 mod prty;
 #[macro_use]
